176a65fdd22701ce4f7c4594c59851ffd3eef9fe
Lola JING <guole.jing@gmail.com>
2022-09-04 22:34:22 +0200

    validates produits check already

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -1,3 +1,8 @@
 class Produit < ApplicationRecord
   belongs_to :user
+
+  validates :titre, presence: true, uniqueness: true
+  validates :prix, presence: true, numericality: { only_integer: true }
+  validates :stock, presence: true, numericality: { only_integer: true }
+  validates :marque, presence: true
 end
